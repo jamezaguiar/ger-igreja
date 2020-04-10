236876ac0976a8b0f4a19a9cea52f03d029ff036
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-04-10 11:34:29 -0300

    Test route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,4 +2,10 @@ import { Router } from 'express';
 
 const routes = new Router();
 
+routes.get('/', (req, res) => {
+  return res.json({
+    message: 'Sistema de Gerencia de Membros e Finanças para Igrejas',
+  });
+});
+
 export default routes;
